ada3dea1057059744515ffae484283f74d0fafd2
Matthew Rodusek <matthew.rodusek@gmail.com>
2016-09-06 22:14:11 -0400

    Updated sinle include string_view

--- single_include/bpstd/string_view.hpp
+++ single_include/bpstd/string_view.hpp
@@ -703,11 +703,7 @@ namespace bpstd { // back-port std
   {
     const size_type max_length = pos > m_size ? 0 : m_size - pos;
 
-  #if BFG_COMPILER_EXCEPTIONS_ENABLED
     return pos < m_size ? basic_string_view<CharT,Traits>( m_str + pos, len > max_length ? max_length : len ) : throw std::out_of_range("Index out of range in basic_string_view::substr");
-  #else
-    return basic_string_view<CharT,Traits>( m_str + pos, len > max_length ? max_length : len );
-  #endif
   }
 
   //--------------------------------------------------------------------------
@@ -1114,22 +1110,6 @@ namespace bpstd { // back-port std
     noexcept
   {
     return lhs.compare(rhs) == 0;
-  }
-
-  template<typename CharT, typename Traits, std::size_t N>
-  inline bool operator == ( const basic_string_view<CharT,Traits>& lhs,
-                            CharT (&rhs)[N] )
-    noexcept
-  {
-    return lhs == basic_string_view<CharT,Traits>(rhs);
-  }
-
-  template<typename CharT, typename Traits, std::size_t N>
-  inline bool operator == ( CharT (&lhs)[N],
-                            const basic_string_view<CharT,Traits>& rhs )
-    noexcept
-  {
-    return basic_string_view<CharT,Traits>(lhs) == rhs;
   }
 
   template<typename CharT, typename Traits>
